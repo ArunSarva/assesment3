3b17a7ed8924e516b80137fbd985022f7fb14ff4
ArunSarva <aramegowdaostb1@gmail.com>
2019-06-04 01:11:52 +0530

    assessment 3 commit4

--- js/login_mainpage.js
+++ js/login_mainpage.js
@@ -1,31 +1,19 @@
 function Signup() {
 	location.replace("signup.html")
 }
-function login_mainpage() {
-	 debugger;
-	var reg= /^([A-Za-z0-9_\-\.]){1,}\@([A-Za-z_\-\.]){1,}\.([A-Za-z]){2,4}$/;
-			var adds= document.forms[form_id].elements[email_id].value;
-			if (reg.test(adds)==false) {
-				alert("wrong");
-				document.forms[form_id].elements[email_id].focus();
-				return false;
-			}
-			else
-			{
-				location.replace("home.html")
-			}
-	location.replace("home.html")
+
+function validate(){
+var reg= /^([A-Za-z0-9_\-\.]){1,}\@([A-Za-z_\-\.]){1,}\.([A-Za-z]){2,4}$/;
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( reg.test(username)==true && password == "123"){
+alert ("Login successfully");
+window.location = "home.html"; 
+return false;
 }
-// function validate(form_id,email_id) {
-// 			var reg= /^([A-Za-z0-9_\-\.]){1,}\@([A-Za-z_\-\.]){1,}\.([A-Za-z]){2,4}$/;
-// 			var adds= document.forms[form_id].elements[email_id].value;
-// 			if (reg.test(adds)==false) {
-// 				alert("wrong");
-// 				document.forms[form_id].elements[email_id].focus();
-// 				return false;
-// 			}
-// 			if (true)
-// 			{
-// 				location.replace("home.html")
-// 			}
-// 		}
+else{
+alert("Email ID and password is not match");
+
+return false;
+}
+}
