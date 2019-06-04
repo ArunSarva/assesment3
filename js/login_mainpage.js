a543ada213824a9e0e40a980df73f7cea46f0110
arun <aramegowdaostb1@gmail.com>
2019-06-04 13:59:18 +0530

    final assessment push

--- js/login_mainpage.js
+++ js/login_mainpage.js
@@ -6,14 +6,18 @@ function validate(){
 var reg= /^([A-Za-z0-9_\-\.]){1,}\@([A-Za-z_\-\.]){1,}\.([A-Za-z]){2,4}$/;
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( reg.test(username)==true && password == "123"){
-alert ("Login successfully");
-window.location = "home.html"; 
-return false;
-}
-else{
-alert("Email ID and password is not match");
 
-return false;
+if ( reg.test(username)==true && password == "123"){
+	window.location = "home.html"; 
+	alert ("Login successfully");
+	return false;
+}
+else if (reg.test(username)==false) {
+		document.getElementById("demo").innerHTML ="Invalid Email";	
+}
+
+else
+{
+	document.getElementById("demo1").innerHTML ="Invalid password";
 }
 }
--- js/signup.js
+++ js/signup.js
@@ -4,18 +4,22 @@
        var forms = document.getElementsByClassName('needs-validation');
        var validation = Array.prototype.filter.call(forms, function(form) {
       form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
+        if (form.checkValidity() === true) {
+          
+          location.replace("login_mainpage.html")
+        }
+        else if (form.checkValidity() === false)
+        {
           event.preventDefault();
           event.stopPropagation();
-        }
+          
+        } 
+
         form.classList.add('was-validated');
       }, false);
     });
   }, false);
 })();
-function Signup() {
-  location.replace("home.html")
-}
 function login() {
   location.replace("login_mainpage.html")
 }
